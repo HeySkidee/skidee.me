b12538d6a9b93dbf47b5876381635aa5accd9232
HeySkidee <harshitojha46@gmail.com>
2022-03-22 13:12:49 +0530

    add discord

---
+++ pages/discord.js
@@ -0,0 +1,42 @@
+function discord(){
+	return (<div>
+	<meta charSet="UTF-8" />
+	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta httpEquiv="refresh" content="0; url=https://discord.gg/JcPS3VD8CP" />
+	<meta property="og:url" content="https://skidee.me/discord" />
+	<meta property="twitter:image" content="https://cdn.discordapp.com/attachments/716523839634407436/915655122019819540/final.gif" type="image/gif" />
+	<meta property="og:image:width" content={340} />
+	<meta property="og:image:height" content={210} />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta name="theme-color" content="#bbf1ff" />
+	<title>Skidee | Discord</title>
+  </div>)
+  
+};
+
+export default discord;
+
+
+// var NewComponent = React.createClass({
+//   render: function() {
+//     return (
+      
+//     );
+//   }
+// });
+
+// window.location.href = 'https://discord.gg/JcPS3VD8CP';
+// import { Route, Redirect } from 'react-router'
+
+// <Route exact path="/" render={() => (
+//   loggedIn ? (
+//     <Redirect to="https://discord.gg/JcPS3VD8CP"/>
+//   ) : (
+//     <PublicHomePage/>
+//   )
+// )}/>
+
+// window.onload = function() {
+//     location.href = "https://discord.gg/JcPS3VD8CP";
+// }
