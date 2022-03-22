583e232498111335956513da5d7bebbdc7baf794
HeySkidee <harshitojha46@gmail.com>
2022-03-23 04:13:19 +0530

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,6 @@ const App = ({ Component, pageProps }) => {
   )
 }
 
-export default App;
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
