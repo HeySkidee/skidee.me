7a60d00fde7b9b31ac3ecd54cf8cba8c0a209080
HeySkidee <harshitojha46@gmail.com>
2022-03-23 10:50:02 +0530

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import '../styles/global.css'
-
-
-
 import { useEffect } from 'react'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
@@ -24,7 +22,7 @@ const App = ({ Component, pageProps }) => {
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
@@ -34,7 +32,7 @@ const App = ({ Component, pageProps }) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
