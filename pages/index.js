2e71e580d592efaf703460be1d2f26ab2454c84d
HeySkidee <harshitojha46@gmail.com>
2022-11-15 19:42:24 +0530

    bacc on this bih

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-
-
+          
           I like making random stuff and learning new things
+
         </p>
         {/* <p>
           I'm active on <a href="https://twitter.com/HeySkidee" target="_blank">Twitter</a>, I tweet about my learnings throughout the process and share some cool resources.
