bac47ca38a1e99d705613d983cf7f44730f81cb8
Skidee <59761032+HeySkidee@users.noreply.github.com>
2022-12-13 11:07:58 +0530

    Update and rename apple-music.js to itunes.js

--- pages/itunes.js
+++ pages/itunes.js
@@ -1,15 +1,15 @@
-function apple-music(){
+function itunes(){
     return(
     <div>
         <meta charset="UTF-8" />
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	    <meta http-equiv="refresh" content="0; url=https://music.apple.com/us/artist/skidee/1654479366" />
-	    <meta property="og:url" content="https://skidee.me/apple-music" />
+	    <meta property="og:url" content="https://skidee.me/itunes" />
 
 	    <title>Skidee on Apple Music</title>
      </div>
     )
 };
 
-export default apple-music;
+export default itunes;
