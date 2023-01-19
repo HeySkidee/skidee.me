4b7be2d4921a7762479817491bcd1a0c9aeac432
Skidee <59761032+HeySkidee@users.noreply.github.com>
2023-01-19 19:10:32 +0530

    Update s.js

--- pages/s.js
+++ pages/s.js
@@ -4,7 +4,7 @@ function s(){
       <meta charset="UTF-8" />
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	    <meta http-equiv="refresh" content="0; url=https://heyskidee.github.io/sauce" />
+	    <meta http-equiv="refresh" content="0; url=https://jvx.vercel.app/s" />
 	    <meta property="og:url" content="https://skidee.me/s" />
 
 	    <title>s</title>
