be35ac851207c1eb712890d68f004e910a3a6112
Skidee <59761032+HeySkidee@users.noreply.github.com>
2023-01-19 20:06:17 +0530

    Create spotify.js

---
+++ pages/spotify.js
@@ -0,0 +1,15 @@
+function spotify() {
+    return (
+        <div>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="refresh" content="0; url=https://open.spotify.com/artist/243uTYHGddgFf17pAeI04C" />
+            <meta property="og:url" content="https://skidee.me/spotify" />
+
+            <title>Skidee - Spotify</title>
+        </div>
+    )
+};
+
+export default spotify;
