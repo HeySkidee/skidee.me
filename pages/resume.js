39c23d02d8bb7fd7650af01334ae36d870917224
Skidee <59761032+HeySkidee@users.noreply.github.com>
2023-06-27 10:59:19 +0530

    Create resume.js

---
+++ pages/resume.js
@@ -0,0 +1,15 @@
+function resume(){
+    return(
+    <div>
+        <meta charset="UTF-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	    <meta http-equiv="refresh" content="0; url=3.2 Harshit Kumar Ojha - Resume.pdf" />
+	    <meta property="og:url" content="https://skidee.me/HeySkidee" />
+
+	    <title>Skidee - Resume</title>
+     </div>
+    )
+};
+
+export default resume;
