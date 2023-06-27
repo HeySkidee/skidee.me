9ec9c57dc77b8042c8af2e6e59a9f98b1b8b004c
Skidee <59761032+HeySkidee@users.noreply.github.com>
2023-06-27 11:00:59 +0530

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -4,7 +4,7 @@ function resume(){
         <meta charset="UTF-8" />
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	    <meta http-equiv="refresh" content="0; url=3.2 Harshit Kumar Ojha - Resume.pdf" />
+	    <meta http-equiv="refresh" content="0; url=https://drive.google.com/file/d/1elgAh-Cru2Vxn-uIaAWQ3P_kk5S2jtN2/view" />
 	    <meta property="og:url" content="https://skidee.me/HeySkidee" />
 
 	    <title>Skidee - Resume</title>
